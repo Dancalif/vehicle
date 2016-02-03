6c784f1e4c917f58ff0eade02b999e99f936712d
Dancalif <dancalif@gmail.com>
2016-02-03 12:24:09 -0800

    Update Vehicle app

---
+++ src/test/java/com/sqa/du/vehicle/VehicleTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.sqa.du.vehicle;
+
+import org.junit.Test;
+
+/**
+ * @author dancalif
+ *
+ */
+public class VehicleTest {
+
+	@Test
+	public void test() {
+
+		Vehicle myVehicle = new Vehicle();
+
+		System.out.println("My vehicle 1: " + myVehicle);
+
+	}
+}
